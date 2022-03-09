3662e6068ebc0390942d828fa31ec1522bb28021
alexbrul <42654943+alexbrul@users.noreply.github.com>
2022-03-09 20:19:23 +0100

    Removing unwanted pictures.

--- src/resources/Images.js
+++ src/resources/Images.js
@@ -3,12 +3,4 @@ export default [
     src: "https://drive.google.com/uc?id=1_oA9Sx4D4DhFrYBFQdL0I1CUIz_LhQue",
     text: "Vi hjelper deg!",
   },
-  {
-    src: "https://drive.google.com/uc?id=1rJFs-8So16UCiDag__hG4yyf_RnC08Fa",
-    text: "Consectetur adipisicing elit cillum dolore eu fugiat nulla",
-  },
-  {
-    src: "https://drive.google.com/uc?id=1HO2AGjd_1yyYI4pYTTBmGXBaWHoGSqCl",
-    text: "Asperiores ex animi explicabo cillum dolore eu fugiat nulla",
-  },
 ]
